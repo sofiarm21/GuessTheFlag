ba6dbe716c36d339cec9d8d33f506413c21c6e37
Sofia Rodriguez <sofiaromorales@gmail.com>
2020-10-12 23:43:33 -0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Sofia Rodriguez Morales on 10/12/20.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
